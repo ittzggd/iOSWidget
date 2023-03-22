00ba21677dbbc213e0fc610748dce9b9784b7dcb
Hee Ju Jang <01kathyy@gmail.com>
2023-03-22 17:51:07 +0900

    Initial Commit

---
+++ WidgetTest_API/WidgetTest_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetTest_APIApp.swift
+//  WidgetTest_API
+//
+//  Created by Katherine JANG on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct WidgetTest_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
